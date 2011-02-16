a473a369d3e85d9b69336ba7807589e88139c349
jeff <jeffshea206@gmail.com>
2011-02-15 20:49:51 -0800

    chap 3 page 132

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -3,7 +3,17 @@ class AdsController < ApplicationController
     @ads = Ad.find(:all)
   end  
   
+  def new
+    @ad = Ad.new
+  end
+  
   def show
     @ad = Ad.find(params[:id])
+  end
+  
+  def create
+    @ad = Ad.new(params[:ad])
+    @ad.save  
+    redirect_to "/ads/#{@ad.id}"
   end
 end
